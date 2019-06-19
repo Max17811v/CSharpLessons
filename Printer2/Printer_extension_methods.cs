8ae6377cb7987d613f2c76c190ab1c22d4dc460c
Maksym Boltychev <maksym.boltychev@synapse.com>
2019-06-19 17:12:54 +0300

    Add project files.

---
+++ Printer2/Printer_extension_methods.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Printer1;
+
+namespace Printer2
+{
+    public static class Printer_extension_methods
+    {
+        public static void Print(this Printer printer, string[] strings)
+        {
+            foreach (string n in strings)
+            {
+                printer.Print(n);
+            }
+        }
+
+        public static void Print(this ColourPrinter colourPrinter, string[] strings, int[] colors)
+        {
+            if (strings.Length == colors.Length)
+            {
+                for (int i = 0; i < colors.Length; i++)
+                    colourPrinter.Print(strings[i], colors[i]);
+            }
+            else Console.WriteLine("ERROR: Input arrays have different dimentions!");
+
+        }
+
+        public static void Print(this PhotoPrinter photoPrinter, Photo[] photos)
+        {
+            foreach (Photo n in photos)
+            {
+                photoPrinter.Print(n);
+            }
+        }
+
+    }
+}
---
+++ Printer2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Printer1;
+
+namespace Printer2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer printer = new Printer();
+            ColourPrinter colourPrinter = new ColourPrinter();
+            PhotoPrinter photoPrinter = new PhotoPrinter();
+
+            string[] strings = { "string1", "string2", "string3", "string4", "string5" };
+            int[] colours = { 1, 2, 3, 4, 5 };
+            Photo[] photos = {
+                new Photo { PhotoData = "photo string1" },
+                new Photo { PhotoData = "photo string2" },
+                new Photo { PhotoData = "photo string3" },
+                new Photo { PhotoData = "photo string4" },
+                new Photo { PhotoData = "photo string5" } };
+
+
+            Console.WriteLine("Print string array:");
+            printer.Print(strings);
+            Console.WriteLine("\nPrint colored string array:");
+            colourPrinter.Print(strings, colours);
+            Console.WriteLine("\nPrint photo array:");
+            photoPrinter.Print(photos);
+
+            Console.ReadKey();
+
+        }
+    }
+}
