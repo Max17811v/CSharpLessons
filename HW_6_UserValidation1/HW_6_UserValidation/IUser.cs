25bab0f9c16eb3924a431924eb72ed55eeb7e3fc
Max17811v <51681403+Max17811v@users.noreply.github.com>
2019-06-20 09:55:01 +0300

    Add files via upload

---
+++ HW_6_UserValidation1/HW_6_UserValidation/IUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_6_UserValidation
+{
+    interface IUser
+    {
+        string Name { get; set; }
+        string Password { get; set; }
+        string Email { get; set; }
+
+        string GetFullInfo();
+    }
+}
---
+++ HW_6_UserValidation1/HW_6_UserValidation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_6_UserValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                User user = new User();
+
+                Console.Write("Enter user Name or Email:");
+                user.Name = Console.ReadLine();
+                Console.Write("\nEnter Password:");
+                user.Password = Console.ReadLine();
+
+                user.ValidateUser();
+            }
+        }
+    }
+}
