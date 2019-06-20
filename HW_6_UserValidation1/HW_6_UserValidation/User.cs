0a26c86bc6de103ff492ce12d110ed28ecde162e
Max17811v <51681403+Max17811v@users.noreply.github.com>
2019-06-20 17:30:17 +0300

    Update User.cs

--- HW_6_UserValidation1/HW_6_UserValidation/User.cs
+++ HW_6_UserValidation1/HW_6_UserValidation/User.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -9,54 +9,12 @@ namespace HW_6_UserValidation
     class User : IUser
     {
         public string Name { get; set; }
-        public string Email { get; set; }
         public string Password { get; set; }
         public DateTime DateTime { get; set; }
-        public static List<User> UsersList { get; set; } = new List<User>
-        {
-            new User { Name = "John", Password = "123", DateTime = DateTime.MinValue }
-        };
-        
-        public void ValidateUser()
-        {
-            if ((this.Name == "exit") && (this.Password == "exit"))
-            {
-                Console.WriteLine("\nYou entered ExitCode and program will exit in 5 seconds");
-                Thread.Sleep(5000);
-                Environment.Exit(0);
-            }
-
-            var IsPresent = false;
-
-            foreach (User i in UsersList)
-            {
-                if (this.Name == i.Name)
-                {
-                    IsPresent = true;
-                    if (this.Password == i.Password)
-                    {
-                        Console.WriteLine($"\nYour last wisit was:{i.DateTime}\n");
-                        i.DateTime = DateTime.Now;
-                        break;
-                    }
-                    else Console.WriteLine($"\nYour entered incorrect password for {this.Name} user.\n"); break;
-
-                }
-                
-            }
-
-            if(!IsPresent)
-            {
-                Console.WriteLine($"\nYour entered new '{this.Name}' user and he will be added to user list.\n");
-                UsersList.Add(new User { Name = this.Name, Password = this.Password, DateTime = DateTime.Now });
-            }
-            
-        }
-
         public string GetFullInfo()
         {
             return "User Name: " + this.Name + " Password: "  
-                + this.Password + " Email: " + this.Email + " Last visit time: " + this.DateTime;
+                + this.Password + " Email: " + this.DateTime;
         }
     }
 }
