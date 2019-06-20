8d6ff55286622a2fc238ae3c35190394c3303f5b
Max17811v <51681403+Max17811v@users.noreply.github.com>
2019-06-20 23:17:04 +0300

    Add files via upload

---
+++ Printer2/Printer/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Printer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Photo photo = new Photo { PhotoData = "photo string" };
+            Printer printer = new Printer();
+            ColourPrinter colourPrinter = new ColourPrinter();
+            PhotoPrinter photoPrinter = new PhotoPrinter();
+            
+
+            printer.Print("Regular printer prints string.\n");
+            colourPrinter.Print("Colour printer prints string.\n");
+            photoPrinter.Print("Photo printer prints string.\n");
+            colourPrinter.Print($"Colour printer prints string with selected {(ConsoleColor)12} colour.\n", 12);
+            photoPrinter.Print(photo);
+
+            Console.ReadKey();
+
+        }
+    }
+
+    public class Printer
+
+    {
+        public virtual void Print(string n)
+        {
+            Console.WriteLine(n);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+    }
+
+    public class ColourPrinter : Printer
+    {
+        public override void Print(string n)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            base.Print(n);
+        }
+
+        public void Print(string n, int color)
+        {
+            Console.ForegroundColor = (ConsoleColor)color;
+            base.Print(n);
+        }
+        
+    }
+
+    public class PhotoPrinter : Printer
+    {
+        public override void Print(string n)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            base.Print(n);
+        }
+
+        public void Print(Photo photo)
+        {
+            Console.WriteLine($"Photo printer prints photo: '{photo.PhotoData}'.\n");
+        }
+
+    }
+
+    public class Photo
+    {
+        public string PhotoData { get; set; }
+
+    }
+       
+}
