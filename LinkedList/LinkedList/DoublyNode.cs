5b8b4d2b69d3ba8e497fca7494b9071c914c7768
Max17811v <51681403+Max17811v@users.noreply.github.com>
2019-06-25 09:29:07 +0300

    Add files via upload

---
+++ LinkedList/LinkedList/DoublyNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class DoublyNode<T>
+    {
+        public DoublyNode(T data)
+        {
+            Data = data;
+        }
+        public T Data { get; set; }
+        public DoublyNode<T> Previous { get; set; }
+        public DoublyNode<T> Next { get; set; }
+    }
+}
---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinkedList<string> linkedList = new DoublyLinkedList<string>();
+            linkedList.Add("Bob");
+            linkedList.Add("Bill");
+            linkedList.Add("Tom");
+            linkedList.AddFirst("Kate");
+            linkedList.Add("Max");
+            linkedList.Add("Crab");
+            Console.WriteLine("Initial linked list:");
+            foreach (var item in linkedList)
+            {
+                Console.Write(item + " ");
+            }
+            linkedList.Remove("Max");
+            Console.WriteLine("\n\nLinked list with removed 'Max':");
+            foreach (var item in linkedList)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("\n\nLinked list element count: " + linkedList.Count);
+            Console.WriteLine("\nDoes the list contain Kate?: " + linkedList.Contains("Kate"));
+            Console.WriteLine("\nLinked list transformed to array:");
+            var arr = linkedList.ToArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i].Data + " ");
+            }
+            //foreach (var i in arr)
+            //{
+            //    Console.Write(i.Data + " ");
+            //}
+            Console.ReadLine();
+        }
+    }
+}
